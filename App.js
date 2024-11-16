328f645fdfb8c58caf39fb18b66fcbd0f870094a
priyanka lahoti <lahotip4@gmail.com>
2024-11-16 14:31:13 +0530

    add initial code files

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from react!");
+const parent = React.createElement(
+    "div",
+{id: "parent"},
+React.createElement(
+    "div",
+    {id: "child"},
+    [React.createElement("h1", {}, "1 am h1"), React.createElement("h2",{},"I am h2")]
+))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+
+root.render(parent);
